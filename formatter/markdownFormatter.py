23752ffa8c15a7e09c68dbff1c787a16e46dee61
gregoire <gregoire_brebner@hotmail.com>
2024-08-18 18:02:53 +0200

    Improve formatting

--- formatter/markdownFormatter.py
+++ formatter/markdownFormatter.py
@@ -7,16 +7,11 @@ def format(p:Parser) -> str:
 # {p.title()}
 
 <{p.url()}>     
-{opt('Author ', p.author(), '   ')}
-{opt('Published ', p.datePublished(), '   ')}  
-{opt('Modified ', p.datePublished(), '   ')}  
+{optList([opt('Author ', p.author()), opt('Published ', p.datePublished()), opt('Modified ', p.datePublished())])}  
 
 {p.description()}  
 
-{opt('Yields: ', p.recipeYield(), '   ')}
-{opt('Prep: ', p.prepTime(), '   ')}
-{opt('Cooking time: ', p.cookTime(), '   ')}
-{opt('Total: ', p.totalTime())}
+{optList([opt('Yields: ', p.recipeYield()), opt('Prep: ', p.prepTime()), opt('Cooking time: ', p.cookTime()), opt('Total: ', p.totalTime())])}
 
 ## Ingredients
 
@@ -24,8 +19,7 @@ def format(p:Parser) -> str:
 
 ## Instructions
 
-{itemised(p.steps())}
-    
+{itemised(p.steps())}    
 """
     
 def opt(prefix: str, value: str, postfix:str = '') -> str:
@@ -33,11 +27,15 @@ def opt(prefix: str, value: str, postfix:str = '') -> str:
         return ''
     return f'{prefix}{value}{postfix}'
 
+def optList(l:list) -> str:
+    l = [e for e in l if e]
+    return '   \n'.join(l)
+
 def bulletPoints(l: list) -> str:
     ret = ''
     for elem in l:
         ret += f' - {elem}\n'
-    return ret;
+    return ret
 
 def itemised(l: list) -> str:
     ret = ''
